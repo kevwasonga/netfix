4958180e1c6eec3901bee92cf729608d10f91074
kevwasonga <kevinwasonga116@gmail.com>
2025-02-20 17:32:59 +0300

    reconfigure cutom login view

--- netfix/urls.py
+++ netfix/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import include, path
 
 from . import views as v
-
+from users.views import CustomLoginView  
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('services/', include('services.urls')),
     path('register/', include('users.urls')),
     path('customer/<slug:name>', v.customer_profile, name='customer_profile'),
-    path('company/<slug:name>', v.company_profile, name='company_profile')
+    path('company/<slug:name>', v.company_profile, name='company_profile'),
+    path("login/", CustomLoginView.as_view(), name="CustomLoginView"),
+
 ]
