4cbac5ef96cbced4177322b71e89eff199d3dfa0
kevwasonga <kevinwasonga116@gmail.com>
2025-02-25 17:52:50 +0300

    reconfigure the registration urls to caputre  users(which is the app handling registrations) instead of capturing 'registration' as the path to be folowed during registration

--- netfix/urls.py
+++ netfix/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('services/', include('services.urls')),
-    path('register/', include('users.urls')),
+    path('users/', include('users.urls')),
     path('customer/<slug:name>', v.customer_profile, name='customer_profile'),
     path('company/<slug:name>', v.company_profile, name='company_profile'),
     path("login/", CustomLoginView.as_view(), name="CustomLoginView"),
--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views
 from .forms import UserLoginForm
 from . import views as v
 urlpatterns = [
-    path('company/', v.CompanySignUpView.as_view(), name='register_company'),
-    path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
+    path('register_company/', v.CompanySignUpView.as_view(), name='register_company'),
+    path('register_customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
     path("profile/<int:pk>/", v.UserProfileView.as_view(), name="user_profile"),
     path('', v.register, name='register'),
 
