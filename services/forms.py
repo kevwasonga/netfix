0d47ced548a477c11bb5fb22a3e84abb1904c2f7
kevwasonga <kevinwasonga116@gmail.com>
2025-02-25 13:22:13 +0300

    update forms to match the exact fields we have on our models

--- services/forms.py
+++ services/forms.py
@@ -1,14 +1,15 @@
 from django import forms
-
-from users.models import Company
+from .models import RequestService
+from .models import Company, Service
+from users.forms import  CompanySignUpForm
 
 
 class CreateNewService(forms.Form):
     name = forms.CharField(max_length=40)
     description = forms.CharField(widget=forms.Textarea, label='Description')
     price_hour = forms.DecimalField(
-        decimal_places=2, max_digits=5, min_value=0.00)
-    field = forms.ChoiceField(required=True)
+    decimal_places=2, max_digits=5, min_value=0.00)
+    field = forms.ChoiceField(choices=Service.choices, required=True)  
 
     def __init__(self, *args, choices='', ** kwargs):
         super(CreateNewService, self).__init__(*args, **kwargs)
@@ -23,5 +24,15 @@ class CreateNewService(forms.Form):
         self.fields['name'].widget.attrs['autocomplete'] = 'off'
 
 
-class RequestServiceForm(forms.Form):
-    pass
+
+
+
+class RequestServiceForm(forms.ModelForm):
+    class Meta:
+        model = RequestService
+        fields = ['address', 'hours']
+        widgets = {
+            'address': forms.Textarea(attrs={'placeholder': 'Enter Address', 'rows': 2}),
+            'hours': forms.NumberInput(attrs={'min': 1}),
+        }
+
