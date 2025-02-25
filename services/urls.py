a774b93817970ef66fb22ae859552b552b65ea4a
kevwasonga <kevinwasonga116@gmail.com>
2025-02-25 10:29:48 +0300

    update service request url to match  views

--- services/urls.py
+++ services/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', v.service_list, name='service_list'),
     path('create/', v.create, name='create'),
     path('<int:id>', v.index, name='index'),
-    path('<int:id>/request_service/', v.request_service, name='request_service'),
+    path('<int:service_id>/request_service/', v.request_service, name='request_service'),
     path('<slug:field>/', v.service_field, name='services_field'),
 ]
