03814c2d6db41d78ab3ab8a0334f108041cca92f
kevwasonga <kevinwasonga116@gmail.com>
2025-03-04 16:15:55 +0300

    Feat: limit only customers to request the  services displayed on the frequently requested services pannel

--- main/views.py
+++ main/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth import logout as django_logout
 
+from django.shortcuts import render
+from django.db.models import Count
+from services.models import RequestService  # Import your model
+
 
 def home(request):
-    return render(request, "main/home.html", {})
+    # Fetch the top 5 most requested services
+    top_services = (
+        RequestService.objects.values('service__id', 'service__name')
+        .annotate(request_count=Count('service'))
+        .order_by('-request_count')[:5]
+    )
+
+    # Determine if the user is allowed to request a service (only customers)
+    is_customer = request.user.is_authenticated and getattr(request.user, 'is_customer', False)
+
+    return render(request, 'main/home.html', {
+        'top_services': top_services,
+        'is_customer': is_customer  # Pass user type to the template
+    })
 
 
 def logout(request):
